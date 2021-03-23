d3a32722a3e58374d2f127402680871ae573d7a4
Ten Artem <ten.ar@citilink.ru>
2021-03-23 12:54:09 +0300

    updated version in go.mod and readme.md

--- processor/processor.go
+++ processor/processor.go
@@ -7,7 +7,7 @@ import (
 	"runtime/debug"
 	"time"
 
-	camundaclientgo "github.com/citilinkru/camunda-client-go"
+	camundaclientgo "github.com/citilinkru/camunda-client-go/v2"
 )
 
 // Processor external task processor
